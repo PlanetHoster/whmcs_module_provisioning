6bc238be1d355d0e600c2c111799c203245c6686
Normand Lemay <normandl@dev.planethoster.info>
2025-07-30 16:49:10 -0400

    Correctif SSO WHMCS : retour du format attendu ['success'=>true, 'redirectTo'=>url] pour ServiceSingleSignOn et AdminSingleSignOn. Correction du nom de la fonction dans base.php. Le SSO fonctionne désormais côté client et admin. #support #whmcs #sso

--- app/Http/Actions/AdminSingleSignOn.php
+++ app/Http/Actions/AdminSingleSignOn.php
@@ -2,12 +2,44 @@
 
 namespace ModulesGarden\PlanetHoster\App\Http\Actions;
 
+use ModulesGarden\PlanetHoster\App\Libs\PlanetHosterAPI;
 use ModulesGarden\PlanetHoster\Core\App\Controllers\Instances\Server\Action;
 
 class AdminSingleSignOn extends Action
 {
     public function execute($params = null)
     {
+        try {
+            if (empty($params['customfields']['account_id'])) {
+                return [
+                    'success' => false,
+                    'error' => 'Custom Field /Account ID/ is not empty'
+                ];
+            }
 
+            $api = new PlanetHosterAPI(
+                $params['serverhostname'],
+                $params['serverusername'],
+                $params['serverpassword'],
+                $params['serverhttpprefix']
+            );
+
+            $result = $api->loginPanel($params['customfields']['account_id']);
+            if (!empty($result['data']['login_url'])) {
+                return [
+                    'success' => true,
+                    'redirectTo' => $result['data']['login_url']
+                ];
+            }
+            return [
+                'success' => false,
+                'error' => 'SSO URL not found'
+            ];
+        } catch (\Exception $e) {
+            return [
+                'success' => false,
+                'error' => $e->getMessage()
+            ];
+        }
     }
 }
